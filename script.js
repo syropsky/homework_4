50a70d28c507e3c8e325964a9d9d61a4fc032f81
syrop <example@gmail.com>
2020-05-04 01:54:56 +0200

    homework_4

---
+++ script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+
+let money, time;
+
+function start() {
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", ""); 
+    }
+
+}
+start();
+
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true,
+    chooseExpenses: function(){
+        for (let i = 0; i < 2; i++) {
+            let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+                b = prompt ("Во сколько обойдется?", "");
+        
+            if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
+        
+        }
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+    },
+    detectLevel: function(){
+        if (appData.moneyPerDay < 100) {
+            console.log ("Это минимальный уровень достатка!");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log ("Это средний уровень достатка!");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log ("Это высокий уровень достатка!");
+        } else {
+            console.log ("Ошибочка...!");
+        }
+    },
+    checkSavings: function(){
+        if (appData.savings == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+    
+                appData.monthIncome = save/100/12*percent;
+                alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function(){
+        for (let i = 1; i <= 3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов?");
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
+        }
+    },
+    chooseIncome: function(){
+        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+        
+        while (!isNaN(items) || items == "" || items == null) {
+            items = prompt ('Что принесет дополнительный доход? (Перечислите через запятую)', ''); 
+        }
+        appData.income = items.split(', ');
+        appData.income.push(prompt('Что-то еще?'));
+        appData.income.sort();
+
+        let addIncome = "";
+        appData.income.forEach(function(items, i, mass){
+            addIncome=addIncome+(i+1 + ': ' + items + " ");
+        });
+        
+        alert("Способы доп заработка: "+addIncome);
+      
+    }
+
+};
+
+let k = "";
+for(let key in appData){
+    k=k+key+", ";
+}
+
+console.log("Наша программа включает в себя данные: "+k);
